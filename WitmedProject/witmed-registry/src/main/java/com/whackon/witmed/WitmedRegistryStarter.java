c9e6f2ea155f5960f134ef480cdb30b42faad429
赵远 <1739916557@qq.com>
2021-12-29 23:57:40 +0800

    创建完成 witmed registry 注册中心模块

---
+++ WitmedProject/witmed-registry/src/main/java/com/whackon/witmed/WitmedRegistryStarter.java
@@ -0,0 +1,21 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <b>注册中心启动类</b>
+ *
+ * @author zyuan
+ * @date 2021/12/29
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class WitmedRegistryStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(WitmedRegistryStarter.class, args);
+	}
+}
