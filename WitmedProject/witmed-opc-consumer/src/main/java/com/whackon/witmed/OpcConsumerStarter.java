91c7ebb4e1a0276667b262532f81382732c6800d
赵远 <1739916557@qq.com>
2022-01-19 22:56:31 +0800

    添加 witmed-opc-consumer

--- WitmedProject/witmed-opc-consumer/src/main/java/com/whackon/witmed/OpcConsumerStarter.java
+++ WitmedProject/witmed-opc-consumer/src/main/java/com/whackon/witmed/OpcConsumerStarter.java
@@ -6,18 +6,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * <b>智慧医疗信息平台 - 系统功能模块 - Consumer 启动类</b>
+ * <b>智慧医疗信息平台 - 门诊功能模块 - Consumer 启动类</b>
  *
- * @author Arthur
- * @date 2021/12/30
+ * @author zyuan
+ * @date 2022/01/13
  * @version 1.0.0
  * @since 1.0.0
  */
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SystemConsumerStarter {
+public class OpcConsumerStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(SystemConsumerStarter.class, args);
+		SpringApplication.run(OpcConsumerStarter.class, args);
 	}
 }
