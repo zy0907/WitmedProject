384dcce3bdf7a786d1691bb0c98eeedae73bd248
赵远 <1739916557@qq.com>
2022-01-19 22:57:55 +0800

    添加 witmed-opc-provider

--- WitmedProject/witmed-opc-provider/src/main/java/com/whackon/witmed/OpcProviderStarter.java
+++ WitmedProject/witmed-opc-provider/src/main/java/com/whackon/witmed/OpcProviderStarter.java
@@ -6,18 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * <b>智慧医疗信息平台 - 系统功能模块 - Provider 启动类</b>
+ * <b>智慧医疗信息平台 - 门诊功能模块 - Provider 启动类</b>
  *
  * @author zyuan
- * @date 2021/12/30
+ * @date 2022/01/13
  * @version 1.0.0
  * @since 1.0.0
  */
-@MapperScan("com.whackon.witmed.*.dao")
+@MapperScan("com.whackon.witmed.system.*.dao")
 @EnableEurekaClient
 @SpringBootApplication
-public class SystemProviderStarter {
+public class OpcProviderStarter {
 	public static void main(String[] args) {
-		SpringApplication.run(SystemProviderStarter.class, args);
+		SpringApplication.run(OpcProviderStarter.class, args);
 	}
 }
